74f5946d30218c7989d09847fe3a3d5f2fa20b38
shango100 <83172130+shango100@users.noreply.github.com>
2023-11-21 19:51:05 +0100

    sound bug fixed
    fixed sound playing problem and css

--- src/components/DrumMachine.js
+++ src/components/DrumMachine.js
@@ -1,31 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/DrumMachine.css'; // Adjust the path based on your actual file structure
-import '../assets/Heater-1.mp3'
-import '../assets/Heater-2.mp3'
+import song1 from "../assets/Heater-1.mp3";
+import song2 from "../assets/Heater-2.mp3";
+import song3 from "../assets/Heater-3.mp3";
+import song4 from "../assets/Heater-4.mp3";
+import song5 from "../assets/Kick_n_Hat.mp3";
+import song6 from "../assets/Open-HH.mp3";
+import song7 from "../assets/RP4_KICK_1.mp3";
+import song8 from "../assets/Clap.mp3";
+import song9 from "../assets/Closed-HH.mp3";
 
 const DrumMachine = () => {
     const [displayText, setDisplayText] = useState('');
 
     const drumPads = [
-        { id: 'Q', text: 'Kick', audioPath: '../assets/Heater-2.mp3' },
-        { id: 'W', text: 'Snare', audioPath: '../assets/Heater-2.mp3' },
-        { id: 'E', text: 'Hi-Hat', audioPath: '/assets/Heater-3.mp3' },
-        { id: 'A', text: 'Tom 1', audioPath: '/assets/Heater-4.mp3' },
-        { id: 'S', text: 'Tom 2', audioPath: '/assets/Clap.mp3' },
-        { id: 'D', text: 'Tom 3', audioPath: '/assets/Open-HH.mp3' },
-        { id: 'Z', text: 'Clap', audioPath: '/assets/Kick_n_Hat.mp3' },
-        { id: 'X', text: 'Crash', audioPath: '/assets/Closed-HH.mp3' },
-        { id: 'C', text: 'Ride', audioPath: '/assets/RP4_KICK_1.mp3' },
+        { id: 'Q', text: 'Kick', audioPath: song1 },
+        { id: 'W', text: 'Snare', audioPath: song2 },
+        { id: 'E', text: 'Hi-Hat', audioPath: song3 },
+        { id: 'A', text: 'Tom 1', audioPath: song4 },
+        { id: 'S', text: 'Tom 2', audioPath: song5},
+        { id: 'D', text: 'Tom 3', audioPath: song6 },
+        { id: 'Z', text: 'Clap', audioPath: song7 },
+        { id: 'X', text: 'Crash', audioPath: song8 },
+        { id: 'C', text: 'Ride', audioPath: song9 },
     ];
 
     const handlePadClick = (audioId, displayText) => {
         const audioElement = document.getElementById(audioId).querySelector('audio');
-        console.log(audioElement); // Log the audioElement to the console
         audioElement.currentTime = 0;
         audioElement.play().then(r => console.log("error with source"));
         setDisplayText(displayText);
     };
-
 
     const handleKeyPress = (event) => {
         const key = event.key.toUpperCase();
@@ -33,7 +38,7 @@ const DrumMachine = () => {
 
         if (drumPad) {
             const audioId = drumPad.getAttribute('id');
-            const displayText = drumPad.getAttribute('data-text'); // Use correct attribute name
+            const displayText = drumPad.getAttribute('data-text');
             handlePadClick(audioId, displayText);
         }
     };
@@ -47,25 +52,25 @@ const DrumMachine = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-
     return (
         <div id="drum-machine">
             <div id="display">{displayText}</div>
-            {drumPads.map((pad) => (
-                <div
-                    key={pad.id}
-                    className="drum-pad"
-                    id={pad.id}
-                    data-text={pad.text}
-                    onClick={() => handlePadClick(pad.id, pad.text)}
-                >
-                    {pad.id}
-                    <audio className="clip" id={pad.id} src={pad.audioPath} />
-                    {/*<audio className="clip" id={pad.id} controls>*/}
-                    {/*    <source src={pad.audioPath} type="audio/ogg"/>*/}
-                    {/*</audio>*/}
-                </div>
-            ))}
+            <div className="drum-pad-container">
+                {drumPads.map((pad) => (
+                    <div
+                        key={pad.id}
+                        className="drum-pad"
+                        id={pad.id}
+                        data-text={pad.text}
+                        onClick={() => handlePadClick(pad.id, pad.text)}
+                    >
+                        {pad.id}
+                        <audio className="clip" id={pad.id}>
+                            <source src={pad.audioPath} />
+                        </audio>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -11,6 +11,17 @@ const root = document.getElementById('root') || document.createElement('div');
 root.id = 'root';
 document.body.appendChild(root);
 
+// import all audio files dynamically TODO:{FIx dynamic importation of audio resources}
+// const context = require.context('./', false, /\.(mp3|ogg|wav)$/);
+//
+// const audioFiles = {};
+// context.keys().forEach((filename) => {
+//     const key = filename.replace('./', '').replace(/\.(mp3|ogg|wav)$/, '');
+//     audioFiles[key] = context(filename).default;
+// });
+//
+// export default audioFiles;
+
 const rootElement = document.getElementById('root');
 
 ReactDOM.createRoot(rootElement).render(
